31a0d99de551b25f42cb96caa04a6e10ce8e69eb
Sasja Koning <sasjakoning@hotmail.com>
2023-03-13 19:03:53 +0100

    added: general js for card

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,19 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+
+
+const card = document.querySelector(".card > section")
+
+const root = document.documentElement;
+
+document.addEventListener("mousemove", (e) => {
+    const x = e.clientX - window.innerWidth / 2;
+    const y = e.clientY - window.innerHeight / 2;
+
+
+    root.style.setProperty('--rotateX', `${y * 0.02}deg`);
+    root.style.setProperty('--rotateY', `${x * 0.02}deg`);
+});
