b02555076941b0c602d76f92ef9b9e9bf34ea509
Sasja Koning <sasjakoning@hotmail.com>
2023-03-15 17:42:13 +0100

    added: chat component

---
+++ src/components/chat/Chat.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    import chatBubble from './ChatBubble.vue'
+    import { defineProps, onMounted } from 'vue'
+
+    const props = defineProps(['topic'])
+
+    onMounted(() => {
+        console.log(props.topic)
+    })
+
+
+</script>
+
+<template>
+
+    <article class="slide-in-left" v-if="topic === 'github'">
+        <chatBubble chatDirection="chat-right" content="smth"/>
+        <chatBubble chatDirection="chat-left" content="smth"/>
+        <chatBubble chatDirection="chat-right" content="smth"/>
+
+        <p>{{ topic }}</p>
+    </article>
+
+</template>
+
+
+<style scoped>
+
+    article {
+        gap: 2rem;
+        justify-content: start;
+    }
+
+
+</style>
