5ccf966919a7cf66cd9550d6751a07659bfbd1a0
Sasja Koning <sasjakoning@hotmail.com>
2023-03-15 17:41:06 +0100

    added: router

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
-
-
-
-
-const card = document.querySelector(".card > section")
-
-const root = document.documentElement;
-
-document.addEventListener("mousemove", (e) => {
-    const x = e.clientX - window.innerWidth / 2;
-    const y = e.clientY - window.innerHeight / 2;
-
-
-    root.style.setProperty('--rotateX', `${y * 0.02}deg`);
-    root.style.setProperty('--rotateY', `${x * 0.02}deg`);
-});
+createApp(App).use(router).mount('#app');
---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from '../components/Home.vue'
+import Chat from '../components/chat/Chat.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/chat/:topic',
+        name: 'Chat',
+        props: true,
+        component: Chat
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+})
+
+export default router
